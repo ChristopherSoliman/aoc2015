5c7b2ea82b7b09f7055b592ce2cc1eb571672f45
ChristopherSoliman <csoliman10@gmail.com>
2025-01-14 12:59:28 +0200

    day 08: complete

--- src/days/day08/part1.rs
+++ src/days/day08/part1.rs
@@ -1,3 +1,27 @@
-pub fn run(path: &str) -> u32 {
-    0
+pub fn run(path: &str) -> usize {
+    let input = std::fs::read_to_string(path).expect("File should be there");
+    input.lines().map(|line| length_diff(&line)).sum()
+}
+
+fn length_diff(line: &str) -> usize {
+    let code_length = line.len();
+    let mut mem_length = line.len() - 2;
+
+    let mut i = 1;
+    let chars = line.chars().collect::<Vec<_>>();
+
+    while i + 1 < chars.len() {
+        if chars[i] == '\\' {
+            let k = match chars[i + 1] {
+                '\\' | '"' => 1,
+                'x' if i + 3 < chars.len() => 3,
+                _ => 0,
+            };
+            mem_length -= k;
+            i += k;
+        }
+        i += 1;
+    }
+
+    code_length - mem_length
 }
--- src/days/day08/part2.rs
+++ src/days/day08/part2.rs
@@ -1,3 +1,21 @@
-pub fn run(path: &str) -> u32 {
-    0
+pub fn run(path: &str) -> usize {
+    let input = std::fs::read_to_string(path).expect("File should be there");
+    input.lines().map(|line| length_diff(&line)).sum()
+}
+
+fn length_diff(line: &str) -> usize {
+    let code_length = line.len();
+    let mut mem_length = line.len() + 4;
+
+    let mut i = 1;
+    let chars = line.chars().collect::<Vec<_>>();
+
+    while i + 1 < chars.len() {
+        if chars[i] == '\\' || chars[i] == '"' {
+            mem_length += 1
+        }
+        i += 1;
+    }
+
+    mem_length - code_length
 }
