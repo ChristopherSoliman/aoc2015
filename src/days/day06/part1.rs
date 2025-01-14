4ee0b8b3593038c827c3023db94ad2ce354f7f00
ChristopherSoliman <csoliman10@gmail.com>
2025-01-14 10:41:24 +0200

    day 06: complete

--- src/days/day06/part1.rs
+++ src/days/day06/part1.rs
@@ -1,3 +1,45 @@
-pub fn run(path: &str) -> u32 {
-    0
+pub fn run(path: &str) -> usize {
+    let input = std::fs::read_to_string(path).expect("File should be there");
+
+    let mut lights = [[false; 1000]; 1000];
+
+    for line in input.lines() {
+        let ws = line.split_whitespace().collect::<Vec<_>>();
+        if ws[0] == "toggle" {
+            let (si, sj) = ws[1].split_once(",").unwrap();
+            let (ei, ej) = ws[3].split_once(",").unwrap();
+            let (si, sj) = (si.parse::<usize>().unwrap(), sj.parse::<usize>().unwrap());
+            let (ei, ej) = (ei.parse::<usize>().unwrap(), ej.parse::<usize>().unwrap());
+
+            for i in si..=ei {
+                for j in sj..=ej {
+                    lights[i][j] = !lights[i][j];
+                }
+            }
+        } else {
+            let (si, sj) = ws[2].split_once(",").unwrap();
+            let (ei, ej) = ws[4].split_once(",").unwrap();
+            let (si, sj) = (si.parse::<usize>().unwrap(), sj.parse::<usize>().unwrap());
+            let (ei, ej) = (ei.parse::<usize>().unwrap(), ej.parse::<usize>().unwrap());
+
+            if ws[1] == "on" {
+                for i in si..=ei {
+                    for j in sj..=ej {
+                        lights[i][j] = true;
+                    }
+                }
+            } else {
+                for i in si..=ei {
+                    for j in sj..=ej {
+                        lights[i][j] = false;
+                    }
+                }
+            }
+        }
+    }
+
+    lights
+        .iter()
+        .map(|r| r.iter().filter(|v| **v).count())
+        .sum()
 }
--- src/days/day06/part2.rs
+++ src/days/day06/part2.rs
@@ -1,3 +1,47 @@
-pub fn run(path: &str) -> u32 {
-    0
+pub fn run(path: &str) -> usize {
+    let input = std::fs::read_to_string(path).expect("File should be there");
+
+    let mut lights: [[u8; 1000]; 1000] = [[0; 1000]; 1000];
+
+    for line in input.lines() {
+        let ws = line.split_whitespace().collect::<Vec<_>>();
+        if ws[0] == "toggle" {
+            let (si, sj) = ws[1].split_once(",").unwrap();
+            let (ei, ej) = ws[3].split_once(",").unwrap();
+            let (si, sj) = (si.parse::<usize>().unwrap(), sj.parse::<usize>().unwrap());
+            let (ei, ej) = (ei.parse::<usize>().unwrap(), ej.parse::<usize>().unwrap());
+
+            for i in si..=ei {
+                for j in sj..=ej {
+                    lights[i][j] += 2;
+                }
+            }
+        } else {
+            let (si, sj) = ws[2].split_once(",").unwrap();
+            let (ei, ej) = ws[4].split_once(",").unwrap();
+            let (si, sj) = (si.parse::<usize>().unwrap(), sj.parse::<usize>().unwrap());
+            let (ei, ej) = (ei.parse::<usize>().unwrap(), ej.parse::<usize>().unwrap());
+
+            if ws[1] == "on" {
+                for i in si..=ei {
+                    for j in sj..=ej {
+                        lights[i][j] += 1;
+                    }
+                }
+            } else {
+                for i in si..=ei {
+                    for j in sj..=ej {
+                        if lights[i][j] > 0 {
+                            lights[i][j] -= 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    lights
+        .iter()
+        .map(|r| r.iter().map(|l| *l as usize).sum::<usize>())
+        .sum()
 }
