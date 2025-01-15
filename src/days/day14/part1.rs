d4af706774322dd46cefea8ecb32f96bd53a4fef
ChristopherSoliman <csoliman10@gmail.com>
2025-01-15 13:28:00 +0200

    day 14: complete

--- src/days/day14/part1.rs
+++ src/days/day14/part1.rs
@@ -1,3 +1,38 @@
+struct Reindeer {
+    speed: u32,
+    fly_time: u32,
+    rest_time: u32,
+}
+
+impl Reindeer {
+    pub fn get_distance(&self, time: u32) -> u32 {
+        let mut cycles = time / (self.fly_time + self.rest_time);
+        let mut rem = 0;
+        if TIME % (self.fly_time + self.rest_time) >= self.fly_time {
+            cycles += 1
+        } else {
+            rem = TIME % (self.fly_time + self.rest_time);
+        }
+        self.speed * (self.fly_time * cycles + rem)
+    }
+}
+
+const TIME: u32 = 2503;
+
 pub fn run(path: &str) -> u32 {
-    0
+    let input = std::fs::read_to_string(path).expect("File should be there");
+
+    input
+        .lines()
+        .map(|line| {
+            let spl = line.split_whitespace().collect::<Vec<_>>();
+            let reindeer = Reindeer {
+                speed: spl[3].parse::<u32>().unwrap(),
+                fly_time: spl[6].parse::<u32>().unwrap(),
+                rest_time: spl[13].parse::<u32>().unwrap(),
+            };
+            reindeer.get_distance(TIME)
+        })
+        .max()
+        .unwrap()
 }
--- src/days/day14/part2.rs
+++ src/days/day14/part2.rs
@@ -1,3 +1,51 @@
+struct Reindeer {
+    points: u32,
+    speed: u32,
+    fly_time: u32,
+    rest_time: u32,
+}
+
+impl Reindeer {
+    pub fn get_distance(&self, time: u32) -> u32 {
+        let mut cycles = time / (self.fly_time + self.rest_time);
+        let mut rem = 0;
+        if time % (self.fly_time + self.rest_time) > self.fly_time {
+            cycles += 1
+        } else {
+            rem = time % (self.fly_time + self.rest_time);
+        }
+        self.speed * (self.fly_time * cycles + rem)
+    }
+}
+
+const TIME: u32 = 2503;
+
 pub fn run(path: &str) -> u32 {
-    0
+    let input = std::fs::read_to_string(path).expect("File should be there");
+    let mut reindeer = vec![];
+    for line in input.lines() {
+        let spl = line.split_whitespace().collect::<Vec<_>>();
+        reindeer.push(Reindeer {
+            points: 0,
+            speed: spl[3].parse::<u32>().unwrap(),
+            fly_time: spl[6].parse::<u32>().unwrap(),
+            rest_time: spl[13].parse::<u32>().unwrap(),
+        });
+    }
+
+    for i in 1..=TIME {
+        let distances = reindeer
+            .iter()
+            .map(|r| r.get_distance(i))
+            .collect::<Vec<_>>();
+        let max = distances.iter().max().unwrap();
+
+        for i in 0..reindeer.len() {
+            if distances[i] == *max {
+                reindeer[i].points += 1;
+            }
+        }
+    }
+
+    reindeer.iter().map(|r| r.points).max().unwrap()
 }
