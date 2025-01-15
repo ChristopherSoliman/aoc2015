17da78506679086d2c9b394ef1c663aebb7b19e8
ChristopherSoliman <csoliman10@gmail.com>
2025-01-15 11:00:38 +0200

    day 12: complete

--- src/days/day12/part1.rs
+++ src/days/day12/part1.rs
@@ -1,32 +1,45 @@
 pub fn run(path: &str) -> i32 {
     let input = std::fs::read_to_string(path).expect("File should be there");
+    let chars = input.chars().collect::<Vec<_>>();
 
     let mut sum = 0;
-    let mut negative = false;
-    let mut cons: Vec<u32> = vec![];
+    let mut i = 0;
 
-    for char in input.chars() {
-        if char.is_digit(10) {
-            cons.push(char.to_digit(10).unwrap());
-        } else {
-            if cons.is_empty() && char == '-' {
-                negative = true;
-            } else {
-                if !cons.is_empty() {
-                    let mut t: i32 = 0;
-                    for i in 0..cons.len() {
-                        t += (cons[i] * 10_u32.pow((cons.len() - i - 1) as u32)) as i32
-                    }
-                    if negative {
-                        t *= -1;
-                    }
-                    sum += t;
-                    cons.clear();
-                }
-                negative = false;
-            }
+    while i < chars.len() {
+        let curr = chars[i];
+        if curr.is_digit(10) || curr == '-' {
+            let (num, dc) = get_number(&chars, i);
+            sum += num;
+            i += dc;
         }
+        i += 1;
     }
 
     sum
+}
+
+fn get_number(chars: &Vec<char>, idx: usize) -> (i32, usize) {
+    let negative = chars[idx] == '-';
+    let mut vals: Vec<u32> = vec![];
+    let mut i = if negative { idx + 1 } else { idx };
+
+    while chars[i].is_digit(10) {
+        vals.push(chars[i].to_digit(10).unwrap());
+        i += 1;
+    }
+
+    if vals.len() == 0 {
+        return (0, 0);
+    }
+
+    let mut sum: i32 = 0;
+    for k in 0..vals.len() {
+        sum += (vals[k] as i32) * 10_i32.pow(vals.len() as u32 - 1 - k as u32);
+    }
+
+    if negative {
+        sum *= -1;
+    }
+
+    (sum, i - idx)
 }
--- src/days/day12/part2.rs
+++ src/days/day12/part2.rs
@@ -1,3 +1,86 @@
-pub fn run(path: &str) -> u32 {
-    0
+pub fn run(path: &str) -> i32 {
+    let input = std::fs::read_to_string(path).expect("File should be there");
+    let chars = input.chars().collect::<Vec<_>>();
+    get_level_sum(&chars, 0).0
+}
+
+fn get_level_sum(chars: &Vec<char>, start: usize) -> (i32, usize) {
+    let mut level = 0;
+    if chars[start] != '{' {
+        return (0, 0);
+    }
+    let mut bad_level = false;
+    let mut sum = 0;
+    let mut i = start + 1;
+
+    loop {
+        let curr = chars[i];
+        if curr == '{' {
+            level += 1;
+            if !bad_level {
+                let (level_sum, dc) = get_level_sum(chars, i);
+                sum += level_sum;
+                i += dc - 1;
+            }
+        } else if curr == '}' {
+            if level == 0 {
+                break;
+            }
+            level -= 1;
+        }
+        if !bad_level {
+            if curr.is_digit(10) || curr == '-' {
+                let (num, dc) = get_number(&chars, i);
+                sum += num;
+                i += dc - 1;
+            }
+            if curr == ':' {
+                if found_red_object(&chars, i) {
+                    bad_level = true;
+                    sum = 0;
+                }
+            }
+        }
+        i += 1;
+    }
+
+    (sum, i - start)
+}
+
+fn found_red_object(chars: &Vec<char>, idx: usize) -> bool {
+    if idx + 5 > chars.len() {
+        return false;
+    }
+    return chars[idx] == ':'
+        && chars[idx + 1] == '"'
+        && chars[idx + 2] == 'r'
+        && chars[idx + 3] == 'e'
+        && chars[idx + 4] == 'd'
+        && chars[idx + 5] == '"';
+}
+
+fn get_number(chars: &Vec<char>, idx: usize) -> (i32, usize) {
+    let negative = chars[idx] == '-';
+    let mut vals: Vec<u32> = vec![];
+    let mut i = if negative { idx + 1 } else { idx };
+
+    while chars[i].is_digit(10) {
+        vals.push(chars[i].to_digit(10).unwrap());
+        i += 1;
+    }
+
+    if vals.len() == 0 {
+        return (0, 0);
+    }
+
+    let mut sum: i32 = 0;
+    for k in 0..vals.len() {
+        sum += (vals[k] as i32) * 10_i32.pow(vals.len() as u32 - 1 - k as u32);
+    }
+
+    if negative {
+        sum *= -1;
+    }
+
+    (sum, i - idx)
 }
