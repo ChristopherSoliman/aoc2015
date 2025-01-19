6314dc3e343938cebb4533707b7f69458e193922
ChristopherSoliman <csoliman10@gmail.com>
2025-01-19 11:00:55 +0200

    aoc 2015: complete

--- src/days/day25/mod.rs
+++ src/days/day25/mod.rs
@@ -1,10 +1,8 @@
 #![allow(dead_code)]
 mod part1;
-mod part2;
 
 pub fn run() {
     println!("====================Day 25====================");
     println!("Part 1: {}", part1::run());
-    println!("Part 2: {}", part2::run());
     println!("==============================================");
 }
